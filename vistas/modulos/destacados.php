88b4a25b44cd2117934f3c2d9e12f3df5d2d1933
javier <jjzoto@gmail.com>
2019-01-22 10:30:26 -0500

    VersionEcommerce/ramaMaster

---
+++ vistas/modulos/destacados.php
@@ -0,0 +1,54 @@
+<!-- BANNER -->
+
+<figure class="banner">
+
+    <img src="localhost/wilredbk/vistas/img/banner/default.jpg" class="img-responsive" alt="">
+
+    <div class="textoBanner textoDer">
+
+        <h1 style="color:#333">La pobreza puede ser superada</h1>
+
+        <h2 style="color:#333"><span>Tu generas una oportunidad</span></h2>
+
+        <h3 style="color:#333">Un niño la recibe</h3>
+
+    </div>
+
+</figure>
+
+<!-- BARRA  -->
+
+<div class="container-fluid well well-sm barraProductos">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-xs-12 organizarProductos">
+
+                <div class="btn-group Pull-right">
+
+                    <button type="button" class="btn btn-default btnGrid" id="btnGrid0">
+                        
+                        <i class="fa fa-th" aria-hidden="true" ></i>
+
+                        <span class="col-xs-0 pull-right">GRID</span>
+
+                    </button>
+
+                    <button type="button" class="btn btn-default btnList" id="btnList0">
+                        
+                        <i class="fa fa-list" aria-hidden="true" ></i>
+
+                        <span class="col-xs-0 pull-right">LIST</span>
+
+                    </button>
+                    
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</div>
